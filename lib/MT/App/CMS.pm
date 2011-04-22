ce9dd7c888b81ea58c5d92c154a413afdfe798d6
Byrne Reese <byrne@majordojo.com>
2011-04-21 23:11:21 -0700

    Squashed warning found in system config area having to do with a sort operation against uninitialized values

--- lib/MT/App/CMS.pm
+++ lib/MT/App/CMS.pm
@@ -2185,7 +2185,7 @@ sub build_menus {
                              }
                   );
             }
-            @sub = sort { $a->{order} <=> $b->{order} } @sub;
+            @sub = sort { ($a->{order} || 0) <=> ($b->{order} || 0) } @sub;
 
             @sub = grep { $_->{allowed} } @sub if $hide_disabled_options;
             if ( !$menu->{mode} ) {
